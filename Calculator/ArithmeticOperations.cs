d3e70016137d63edf0cc38473dd2a4fb7c8aecfd
Kpoxu4 <kpoxu4111@gmail.com>
2024-03-16 13:17:54 +0300

    Добавьте файлы проекта.

---
+++ Calculator/ArithmeticOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class ArithmeticOperations
+    {
+        public static double Arithmetic(double x, double y, char arithmetic) 
+        {
+            double result = 0;
+            switch (arithmetic)
+            {
+                case '+':
+                    result = x + y;
+                    break;
+                case '-':
+                    result = x - y;
+                    break;
+                case '*':
+                    result = x * y;
+                    break;
+                case '/':
+                    if (y != 0)
+                        result = x / y;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using Calculator;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using static System.Collections.Specialized.BitVector32;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Калькулятор");
+    string stringResult = Console.ReadLine();
+    double result = 0;
+    char[] arithmeticOperations = { '+', '-', '*', '/' };
+    var arithmetic = stringResult.IndexOfAny(arithmeticOperations) != -1 ?
+                stringResult[stringResult.IndexOfAny(arithmeticOperations)] : '!';
+
+    if (stringResult.Contains('.'))
+        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+    else
+        Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("Ru");
+
+    if (stringResult.Contains(arithmetic) && arithmetic != '!')
+    {
+        var array = stringResult.Split(arithmetic).ToArray();
+        if (double.TryParse(array[0], out double x) && double.TryParse(array[1], out double y))
+        {
+            result = ArithmeticOperations.Arithmetic(x, y, arithmetic);
+
+            Console.Clear();
+            Console.WriteLine(y == 0 && arithmetic == '/' ? "На ноль делить нельзя" :
+                 $"{stringResult} = {Math.Round(result, 3, MidpointRounding.ToEven)}");
+        }
+        else
+            Console.WriteLine("Ввели не корректные значения цифр"); 
+    }
+    else
+        Console.WriteLine("Ввели не корректные значения арифметического действия");
+
+    Console.WriteLine("для продолжение нажмите любую кнопку");
+    Console.WriteLine("для выхода нажмите ESC");
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Escape)
+        break;
+}        
+    
+
+
